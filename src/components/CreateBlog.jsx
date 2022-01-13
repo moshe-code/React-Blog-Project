3ba3623900b01adb72370c6d2c5bc6aaa99c924f
Moshe Weiner <moshesweiner@gmail.com>
2022-01-13 21:44:08 +0200

    blog-form

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -2,9 +2,25 @@ const CreateBlog = () => {
     return (
         <div className="CreateBlog-root">
             <br></br>
-        <h1>CreateBlog</h1>       
+        <h1>CreateBlog</h1>
+        <div className="blog-container">
+
+            <label htmlFor="blog-title">Title</label>
+            <input type="text" />
+            <br />
+            <label htmlFor="">Author</label>
+            <input type="text" name="" id="" />
+            <br />
+            <label htmlFor="">Body</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <br />
+            <button id="submit-button">Submit</button>
+
+            
+            </div>       
         </div>
-        
+
+
     )
 }
 
