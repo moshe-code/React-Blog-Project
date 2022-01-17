2430a372e96073b44cdaa8e74981e1b13c0a3dc0
Moshe Weiner <moshesweiner@gmail.com>
2022-01-17 20:47:21 +0200

    init stage 5

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
+import { useState } from "react"
+
+
 const Home = () => {
+    const [todoList,setTodoList] = useState([])
+   
+
+    
+    
+    const handleRequestAllTodos = () =>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json())
+        .then(data =>{
+            setTodoList(data);
+        })
+
+        .catch(e => {
+            console.log(e);
+        })
+            
+    
+       
+    }
+
     return (
         <div className="Home-root">
             <br></br>
-            <h1>Home</h1>
-        
+            <h1>To do list</h1> 
+            <button 
+                onClick={handleRequestAllTodos}
+                >Request all todos</button>  
+                <ul>
+                    {
+                        todoList.map(element =>{
+                            return <li key={`todo-item-${element.id}`}>{element.title}</li>
+                        })
+                    }
+                    </ul>            
         </div>
     )
 }
