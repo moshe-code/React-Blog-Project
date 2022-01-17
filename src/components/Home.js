b331580daec2e9977f5e1763d8b2716b5ee663a6
Moshe Weiner <moshesweiner@gmail.com>
2022-01-17 21:33:48 +0200

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
         .then(res => res.json())
         .then(data =>{
+            console.table(data);
             setTodoList(data);
         })
 
@@ -25,14 +26,22 @@ const Home = () => {
     return (
         <div className="Home-root">
             <br></br>
-            <h1>To do list</h1> 
+            <h1 style={{
+            }}>To do list</h1> 
             <button 
                 onClick={handleRequestAllTodos}
                 >Request all todos</button>  
                 <ul>
                     {
                         todoList.map(element =>{
-                            return <li key={`todo-item-${element.id}`}>{element.title}</li>
+                            return <li key={`todo-item-${element.id}`}
+                            style={{
+                                textDecoration: element.completed ? 'line-through' : 'none',
+                                color:element.userId % 2 == 0 ? "blue" : "red"
+
+                            }}
+                            
+                            >{element.title}</li>
                         })
                     }
                     </ul>            
